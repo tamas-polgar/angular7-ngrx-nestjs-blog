26e1863db789aa7548b37ec9d03b105b1dcf8d53
tamas-polgar <tamas.polgar.dev@gmail.com>
2019-04-12 09:58:03 +0200

    -- auth login and logout effects --

--- front/src/app/pages/auth/auth.effects.ts
+++ front/src/app/pages/auth/auth.effects.ts
@@ -7,20 +7,30 @@ import { LoginAction, LogoutAction } from 'src/app/ngrx/actions/auth.actions';
 @Injectable()
 export class AuthEffects {
 
-  @Effect()
-  login$: Observable<any>;
+  @Effect({ dispatch: false })
+  login$: Observable<any> = this.actions$
+    .pipe(
+      ofType((new LoginAction()).type),
+      tap((action: LoginAction) => {
+        // NOTE: effect implementation here
+        console.log('storing:', action);
+        localStorage.setItem('user', JSON.stringify(action.payload.user));
+      })
+    );
 
-  constructor(private actions$: Actions<LoginAction | LogoutAction>) {
-    this.login$ = this.actions$
-      .pipe(
-        ofType((new LoginAction()).type),
-        tap((action: LoginAction) => {
-          console.log('storing:', action);
-          localStorage.setItem('user', JSON.stringify(action.payload.user));
-        })
-      );
-  }
+  @Effect({ dispatch: false })
+  logout$: Observable<any> = this.actions$
+    .pipe(
+      ofType((new LogoutAction()).type),
+      tap((action: LogoutAction) => {
+        // NOTE: effect implementation here
+        console.log('storing:', action);
+        localStorage.removeItem('user');
+      })
+    );
 
-
+  constructor(
+    private actions$: Actions<LoginAction | LogoutAction>
+  ) { }
 
 }
