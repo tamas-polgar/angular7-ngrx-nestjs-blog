e78412786a79f3c4433755e96623dbfe374199d8
tamas-polgar <tamas.polgar.dev@gmail.com>
2019-04-16 14:12:26 +0200

    -- add logger for ngrx --

--- front/src/app/ngrx/reducers/index.ts
+++ front/src/app/ngrx/reducers/index.ts
@@ -1,8 +1,17 @@
-import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { ActionReducer, ActionReducerMap, MetaReducer, State } from '@ngrx/store';
 import { storeFreeze } from 'ngrx-store-freeze';
+import { storeLogger } from 'ngrx-store-logger';
 
 import { environment } from '../../../environments/environment';
 
+function appLogger(reducer: ActionReducer<State<any>>): any {
+  // default, no options
+  return storeLogger({
+    collapsed: true
+  })(reducer);
+}
+
+// ----------------------------------------
 
 export class AppState {
   // NOTE: this is filled by the lazy loaded modules
@@ -12,4 +21,6 @@ export const reducers: ActionReducerMap<AppState> = {
   // NOTE: this is filled by the lazy loaded modules
 };
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [storeFreeze] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production
+  ? [storeFreeze, environment.stateLogger ? appLogger : x => x]
+  : [];
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   apiRoute: 'http://localhost:3000/api/',
+  stateLogger: false
 };
 
 /*
